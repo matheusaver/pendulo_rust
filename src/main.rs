2306ecd001a28d84090145016b8b9eb5220c2679
Matheus Aver <matheusaver@gmail.com>
2024-04-04 16:05:40 -0300

    Pendulo 1.1

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,46 @@
-
 // https://www.youtube.com/watch?v=U-X51GsTAzA
 // Video explicativo sobre este software
 
 use speedy2d::color::Color;
 use speedy2d::window::{WindowHandler, WindowHelper};
-use speedy2d::Graphics2D;
-
+use speedy2d::{Graphics2D, Window};
 
 use vector::Vector;
 
 fn main() {
-    let window: Window = Window::new_centered(title: "Pendulum", size: (800, 480)).unwrap();
-    let win: MyWindowHandler = MyWindowHandler{
-        p: Pendulum::new(x: 400.0, y: 0.0, r: 200.0),
+    let window: Window = Window::new_centered("Pendulum", (800, 480)).unwrap();
+    let win: MyWindowHandler = MyWindowHandler {
+        p: Pendulum::new(400.0, 0.0, 200.0),
+        p2: Pendulum::new(400.0, 0.0, 300.0),
     };
-    window.run_loop(handler: win);
+    window.run_loop(win);
 }
 
 struct MyWindowHandler {
     p: Pendulum,
+    p2: Pendulum,
 }
 
 impl WindowHandler for MyWindowHandler {
-    fn on_draw(&mut self, helper: &mut WindowHelper, graphics: &mut Graphics2D){
+    fn on_draw(&mut self, helper: &mut WindowHelper, graphics: &mut Graphics2D) {
         graphics.clear_screen(Color::from_rgb(0.8, 0.9, 1.0));
+       
         self.p.update();
         self.p.draw(graphics);
+
+        self.p2.update();
+        self.p2.draw(graphics);      
+       
+       
+        helper.request_redraw();
     }
 }
 
-struct Pendulum{
+struct Pendulum {
     // Este vetor é a posição do pendulo
-    origin: vector::Vector,
+    origin: Vector,
     // Este vetor é a posição da bola
-    position: vector::Vector,
+    position: Vector,
 
     // Angulo do pendulo
     angle: f32,
@@ -45,14 +52,23 @@ struct Pendulum{
     g: f32, // Gravidade
 }
 
-impl Pendulum{
-    fn new(x: f32, y: f32, r: f32) -> Pendulum{
-        Pendulum{ origin: vector::Vector::new(x, y), position: vector::Vector::new(x: 0.0, y 0.0), angle: () angular_velocity: (), angular_acceleration: (), r: (), m: (), g: (),}
+impl Pendulum {
+    fn new(x: f32, y: f32, r: f32) -> Pendulum {
+        Pendulum {
+            origin: Vector::new(x, y),
+            position: Vector::new(0.0, 0.0),
+            angle: 1.0,
+            angular_velocity: 0.0,
+            angular_acceleration: 0.0,
+            r: r,
+            m: 1.0,
+            g: 1.5,
+        }
     }
 
-    fn update(&mut self){
+    fn update(&mut self) {
         // Usando a equação do pendulo para calcular a aceleração angular
-        self.angular_acceleration = -1.0 * self.g * self.angle.sin() / self.r;
+        self.angular_acceleration = -1.0 * (1.0 / self.m) * self.g * self.angle.sin() / self.r;
 
         // Velocidade angular é a velocidade angular mais aceleração angular
         self.angular_velocity += self.angular_acceleration;
@@ -61,37 +77,33 @@ impl Pendulum{
         self.angle += self.angular_velocity;
 
         // A posição é a coordenada polar traduzida em coordenadas cartesianas
-        self.position Vector
-        .set(x: self.r * self.angle.sin(), y: self.r * self.angle.cos())
+        self.position
+            .set(self.r * self.angle.sin(), self.r * self.angle.cos());
 
         // Aposição final da bola é a original mais a posição do vetor
         self.position.add(&self.origin);
-
     }
 
-    fn draw(&self, graphics: &mut Graphics2D){
+    fn draw(&self, graphics: &mut Graphics2D) {
         graphics.draw_line(
-            start_position: (self.origin.x, self.origin.y),
-            end_position: (self.position.x, self.position.y),
-            thickness: 3.0,
-            color: Color::RED,
+            (self.origin.x, self.origin.y),
+            (self.position.x, self.position.y),
+            3.0,
+            Color::RED,
         );
-        graphics.draw_circle(center_position: (self.position.x, self.position.y), radius: 30.0,)
+        graphics.draw_circle((self.position.x, self.position.y), 30.0, Color::BLUE);
     }
 }
 
 mod vector {
-    pub struct Vector{
+    pub struct Vector {
         pub x: f32,
         pub y: f32,
     }
 
-    impl Vector{
-        pub fn new(x: f32, y: f32) -> Vector{
-            Vector{
-                x,
-                y,
-            }
+    impl Vector {
+        pub fn new(x: f32, y: f32) -> Vector {
+            Vector { x, y }
         }
 
         pub fn add(&mut self, other: &Vector) -> &Vector {
@@ -99,7 +111,7 @@ mod vector {
             self.y += other.y;
             self
         }
-        
+
         pub fn set(&mut self, x: f32, y: f32) -> &Vector {
             self.x = x;
             self.y = y;
